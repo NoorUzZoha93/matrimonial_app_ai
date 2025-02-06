31dd7a56a0aa3b6c24d92a5282c8a07e67cb145a
NoorUzZoha93 <noorzoha786@gmail.com>
2025-02-06 12:46:52 +0300

    Update matrimonial_app_code.py

--- matrimonial_app_code.py
+++ matrimonial_app_code.py
@@ -32,6 +32,8 @@ def display_function():
     #         # fetch data from SQLITE
     data = c.execute("select * from matri_users")
     df = pd.DataFrame(data)
+    conn.commit()
+    df = df.drop_duplicates()
     st.write(df)
     conn.commit()
 def Matching_function():
